17ffc5a077e9eb875e03f9efe89bcfb6dae84bce
Nezamkha32 <nezamkha32@gmail.com>
2026-05-03 15:37:27 +0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+                footerpage
+                
+        </div>
+    );
+};
+
+export default Footer;
